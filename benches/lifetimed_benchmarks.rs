0ffbfb7412425259dd9dbff242a8087ba4007532
Mike Hsu <mike.hsu@gmail.com>
2023-10-04 11:07:57 -0700

    do not measure executor setup time

--- benches/lifetimed_benchmarks.rs
+++ benches/lifetimed_benchmarks.rs
@@ -1,4 +1,6 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{
+    criterion_group, criterion_main, measurement::WallTime, BenchmarkGroup, Criterion,
+};
 use std::thread::available_parallelism;
 
 use async_executor::Task;
@@ -10,43 +12,49 @@ const TASKS: usize = 300;
 const STEPS: usize = 300;
 const LIGHT_TASKS: usize = 25_000;
 
-fn spawn_one(b: &mut criterion::Bencher) {
+fn spawn_one(group: &mut BenchmarkGroup<WallTime>) {
     let executor = setup_executor();
-    b.iter(move || {
-        future::block_on(async { executor.spawn(async {}).await });
-    });
-}
-
-fn spawn_many(b: &mut criterion::Bencher) {
-    let executor = setup_executor();
-    b.iter(move || {
-        future::block_on(async {
-            let mut tasks = Vec::new();
-            for _ in 0..LIGHT_TASKS {
-                tasks.push(executor.spawn(async {}));
-            }
-            for task in tasks {
-                task.await;
-            }
+    group.bench_function("spawn_one", |b| {
+        b.iter(|| {
+            future::block_on(async { executor.spawn(async {}).await });
         });
     });
 }
 
-fn yield_now(b: &mut criterion::Bencher) {
+fn spawn_many(group: &mut BenchmarkGroup<WallTime>) {
     let executor = setup_executor();
-    b.iter(move || {
-        future::block_on(async {
-            let mut tasks = Vec::new();
-            for _ in 0..TASKS {
-                tasks.push(executor.spawn(async move {
-                    for _ in 0..STEPS {
-                        future::yield_now().await;
-                    }
-                }));
-            }
-            for task in tasks {
-                task.await;
-            }
+    group.bench_function("spawn_manu", |b| {
+        b.iter(|| {
+            future::block_on(async {
+                let mut tasks = Vec::new();
+                for _ in 0..LIGHT_TASKS {
+                    tasks.push(executor.spawn(async {}));
+                }
+                for task in tasks {
+                    task.await;
+                }
+            });
+        });
+    });
+}
+
+fn yield_now(group: &mut BenchmarkGroup<WallTime>) {
+    let executor = setup_executor();
+    group.bench_function("yield_now", |b| {
+        b.iter(|| {
+            future::block_on(async {
+                let mut tasks = Vec::new();
+                for _ in 0..TASKS {
+                    tasks.push(executor.spawn(async move {
+                        for _ in 0..STEPS {
+                            future::yield_now().await;
+                        }
+                    }));
+                }
+                for task in tasks {
+                    task.await;
+                }
+            });
         });
     });
 }
@@ -69,108 +77,117 @@ fn yield_now(b: &mut criterion::Bencher) {
 //     });
 // }
 
-fn ping_pong(b: &mut criterion::Bencher) {
-    const NUM_PINGS: usize = 1_000;
+fn ping_pong(group: &mut BenchmarkGroup<WallTime>) {
     let executor = setup_executor();
+    group.bench_function("ping_pong", |b| {
+        const NUM_PINGS: usize = 1_000;
 
-    let (send, recv) = async_channel::bounded::<async_oneshot::Sender<_>>(10);
-    let _task: Task<Option<()>> = executor.spawn(async move {
-        loop {
-            let os = recv.recv().await.ok()?;
-            os.send(0u8).ok()?;
-        }
-    });
-    b.iter(|| {
-        let send = send.clone();
-        future::block_on(async move {
-            for _ in 0..NUM_PINGS {
-                let (os_send, os_recv) = async_oneshot::oneshot();
-                send.send(os_send).await.unwrap();
-                os_recv.await.unwrap();
+        let (send, recv) = async_channel::bounded::<async_oneshot::Sender<_>>(10);
+        let _task: Task<Option<()>> = executor.spawn(async move {
+            loop {
+                let os = recv.recv().await.ok()?;
+                os.send(0u8).ok()?;
             }
+        });
+        b.iter(|| {
+            let send = send.clone();
+            future::block_on(async move {
+                for _ in 0..NUM_PINGS {
+                    let (os_send, os_recv) = async_oneshot::oneshot();
+                    send.send(os_send).await.unwrap();
+                    os_recv.await.unwrap();
+                }
+            });
         });
     });
 }
 
-fn fanout(b: &mut criterion::Bencher) {
-    const NUM_TASKS: usize = 1_000;
-    const NUM_ITER: usize = 1_000;
+fn fanout(group: &mut BenchmarkGroup<WallTime>) {
     let executor = setup_executor();
+    group.bench_function("fanout", |b| {
+        const NUM_TASKS: usize = 1_000;
+        const NUM_ITER: usize = 1_000;
 
-    let (send, recv) = async_channel::bounded(1);
-    let _tasks = (0..NUM_TASKS)
-        .map(|_i| {
-            let recv = recv.clone();
-            executor.spawn(async move {
-                for _ctr in 0.. {
-                    if recv.recv().await.is_err() {
-                        return;
+        let (send, recv) = async_channel::bounded(1);
+        let _tasks = (0..NUM_TASKS)
+            .map(|_i| {
+                let recv = recv.clone();
+                executor.spawn(async move {
+                    for _ctr in 0.. {
+                        if recv.recv().await.is_err() {
+                            return;
+                        }
                     }
+                })
+            })
+            .collect::<Vec<_>>();
+        b.iter(|| {
+            let send = send.clone();
+            future::block_on(async move {
+                for _ in 0..NUM_ITER {
+                    send.send(()).await.unwrap();
                 }
             })
         })
-        .collect::<Vec<_>>();
-    b.iter(|| {
-        let send = send.clone();
-        future::block_on(async move {
-            for _ in 0..NUM_ITER {
-                send.send(()).await.unwrap();
-            }
-        })
-    })
+    });
 }
 
-fn context_switch_quiet(b: &mut criterion::Bencher) {
-    let (send, mut recv) = async_channel::bounded::<usize>(1);
-    let mut tasks: Vec<Task<Option<()>>> = vec![];
+fn context_switch_quiet(group: &mut BenchmarkGroup<WallTime>) {
     let executor = setup_executor();
-    for _ in 0..TASKS {
-        let old_recv = recv.clone();
-        let (new_send, new_recv) = async_channel::bounded(1);
-        tasks.push(executor.spawn(async move {
-            loop {
-                new_send.send(old_recv.recv().await.ok()?).await.ok()?
-            }
-        }));
-        recv = new_recv;
-    }
-    b.iter(move || {
-        future::block_on(async {
-            send.send(1).await.unwrap();
-            recv.recv().await.unwrap();
+    group.bench_function("context_switch_quiet", |b| {
+        let (send, mut recv) = async_channel::bounded::<usize>(1);
+        let mut tasks: Vec<Task<Option<()>>> = vec![];
+
+        for _ in 0..TASKS {
+            let old_recv = recv.clone();
+            let (new_send, new_recv) = async_channel::bounded(1);
+            tasks.push(executor.spawn(async move {
+                loop {
+                    new_send.send(old_recv.recv().await.ok()?).await.ok()?
+                }
+            }));
+            recv = new_recv;
+        }
+        b.iter(move || {
+            future::block_on(async {
+                send.send(1).await.unwrap();
+                recv.recv().await.unwrap();
+            });
         });
     });
 }
 
-fn context_switch_busy(b: &mut criterion::Bencher) {
-    let (send, mut recv) = async_channel::bounded::<usize>(1);
-    let mut tasks: Vec<Task<Option<()>>> = vec![];
+fn context_switch_busy(group: &mut BenchmarkGroup<WallTime>) {
     let executor = setup_executor();
-    for _ in 0..TASKS / 10 {
-        let old_recv = recv.clone();
-        let (new_send, new_recv) = async_channel::bounded(1);
-        tasks.push(executor.spawn(async move {
-            loop {
-                // eprintln!("forward {}", num);
-                new_send.send(old_recv.recv().await.ok()?).await.ok()?;
-            }
-        }));
-        recv = new_recv;
-    }
-    for _ in 0..TASKS {
-        tasks.push(executor.spawn(async move {
-            loop {
-                future::yield_now().await;
-            }
-        }))
-    }
-    b.iter(move || {
-        future::block_on(async {
-            for _ in 0..10 {
-                // eprintln!("send");
-                send.send(1).await.unwrap();
-                recv.recv().await.unwrap();
-            }
+    group.bench_function("context_switch_busy", |b| {
+        let (send, mut recv) = async_channel::bounded::<usize>(1);
+        let mut tasks: Vec<Task<Option<()>>> = vec![];
+        for _ in 0..TASKS / 10 {
+            let old_recv = recv.clone();
+            let (new_send, new_recv) = async_channel::bounded(1);
+            tasks.push(executor.spawn(async move {
+                loop {
+                    // eprintln!("forward {}", num);
+                    new_send.send(old_recv.recv().await.ok()?).await.ok()?;
+                }
+            }));
+            recv = new_recv;
+        }
+        for _ in 0..TASKS {
+            tasks.push(executor.spawn(async move {
+                loop {
+                    future::yield_now().await;
+                }
+            }))
+        }
+        b.iter(move || {
+            future::block_on(async {
+                for _ in 0..10 {
+                    // eprintln!("send");
+                    send.send(1).await.unwrap();
+                    recv.recv().await.unwrap();
+                }
+            });
         });
     });
 }
@@ -196,15 +213,15 @@ fn setup_executor() -> Executor<'static> {
 fn criterion_benchmark(c: &mut Criterion) {
     let _ = env_logger::try_init();
     let mut group = c.benchmark_group("lifetimed");
-    group.bench_function("spawn_one", spawn_one);
-    group.bench_function("spawn_many", spawn_many);
-    group.bench_function("yield_now", yield_now);
-    group.bench_function("fanout", fanout);
+    spawn_one(&mut group);
+    spawn_many(&mut group);
+    yield_now(&mut group);
+    fanout(&mut group);
     // c.bench_function("busy_loops", busy_loops);
-    group.bench_function("ping_pong", ping_pong);
+    ping_pong(&mut group);
 
-    group.bench_function("context_switch_quiet", context_switch_quiet);
-    group.bench_function("context_switch_busy", context_switch_busy);
+    context_switch_quiet(&mut group);
+    context_switch_busy(&mut group);
 }
 
 criterion_group!(life_benches, criterion_benchmark);
